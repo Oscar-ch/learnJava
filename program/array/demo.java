c1c67bfea859010d9ac19ae7a5ec0a1b9bf7187b
Oscar <1159098869@qq.com>
2023-08-17 09:47:00 +0800

    8.16学习记录：新增学习二维数组

--- program/array/demo.java
+++ program/array/demo.java
@@ -12,19 +12,15 @@
 
 class ArrayDemo{
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         //数组的不同定义方式
        int[] arr = new int[3];//必须明确长度,默认赋值0
-
        int[] arr2 = new int[]{89,34,5,8};
-
        int[] arr3 = {89,34,5,8};
 
        arr[0] = 9;//取数组中的值的方式，数组名[索引]
 
         System.out.println(arr[0]);
-
         System.out.println(arr);
 
         //对数组进行遍历
